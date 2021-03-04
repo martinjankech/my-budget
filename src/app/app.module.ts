12d743401036254c3dcd73769a5a7200f3d10236
Martin Jankech <jankech3@gmail.com>
2021-03-04 14:56:59 +0100

    easy calc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {FormsModule} from '@angular/forms';
+import { CalcComponent } from './calc/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalcComponent
   ],
   imports: [
     BrowserModule,
