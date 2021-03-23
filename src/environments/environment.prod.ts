1f564092630f2ed5304e8c3912970a9734110afe
Martin Jankech <jankech3@gmail.com>
2021-03-23 12:24:19 +0100

    firebase database

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase:{
+    apiKey: "AIzaSyBvTmx6IbZGum-pDbzWJwnA93iHo5QLVpU",
+    authDomain: "mybudget-64d62.firebaseapp.com",
+    projectId: "mybudget-64d62",
+    storageBucket: "mybudget-64d62.appspot.com",
+    messagingSenderId: "82516735586",
+    appId: "1:82516735586:web:740eea3f040becaa5cc33b",
+    measurementId: "G-HEJ7422Q6D"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyBvTmx6IbZGum-pDbzWJwnA93iHo5QLVpU",
+  authDomain: "mybudget-64d62.firebaseapp.com",
+  projectId: "mybudget-64d62",
+  storageBucket: "mybudget-64d62.appspot.com",
+  messagingSenderId: "82516735586",
+  appId: "1:82516735586:web:740eea3f040becaa5cc33b",
+  measurementId: "G-HEJ7422Q6D"
+}
 };
 
 /*
